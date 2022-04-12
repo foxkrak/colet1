5801f8c27e88b6d47a54010618aba5b6f431eab0
foxkrak <foxkrak@gmail.com>
2022-04-12 13:16:51 -0300

    Update colet.js

--- colet.js
+++ colet.js
@@ -15,6 +15,7 @@ var madeira = 0;
 var stone = 0;
 var iron = 0;
 let ss = 0;
+let confbtn = false;
 ss = JSON.parse(localStorage.getItem('TimerRodando'))
 total = JSON.parse(localStorage.getItem('TotalRecursoColetado'));
 format2 = (new Intl.NumberFormat('dec', { style: 'decimal' }).format(total));
@@ -23,7 +24,7 @@ let format3 = (new Intl.NumberFormat('dec', { style: 'decimal' }).format(totalDi
 document.querySelector('#content_value').querySelector('h2').style.cssText = 'text-align: center'
 document.querySelector('#content_value').querySelector('img').style.display = 'none'
 document.querySelector('#content_value').querySelector('td').style.display = 'none'
-$('#content_value').find('h2').html('[Auto Coleta] Criado por Foxkrak</br></br>Tempo:' + ' ' + timer1(ss) + '⁣⁣      ' + 'Total de Recurso Coletado: ' + format2 + '</br></br>' + 'Madeira: ' + format3 + '&nbsp;&nbsp;&nbsp;Argila: ' + format3+ '&nbsp;&nbsp;&nbsp;Ferro: ' + format3)
+$('#content_value').find('h2').html('[Auto Coleta] Criado por Foxkrak</br></br>Tempo:' + ' ' + timer1(ss) + '⁣⁣      ' + 'Total de Recurso Coletado: ' + format2 + '</br></br>' + 'Madeira: ' + format3 + '&nbsp;&nbsp;&nbsp;Argila: ' + format3+ '&nbsp;&nbsp;&nbsp;Ferro: ' + format3)
 
 function resetar(){
     total = JSON.parse(localStorage.getItem('TotalRecursoColetado'));
@@ -60,16 +61,18 @@ document.querySelector('.opcoestd').children[1].appendChild(createEle('td'))
 document.querySelector('.opcoestd').children[1].children[0].appendChild(createEle('button','Iniciar','iniciarBtn'))
 document.querySelector('.opcoestd').children[1].children[0].appendChild(createEle('button','Pausar','pausarBtn'))
 document.querySelector('.opcoestd').children[1].children[0].appendChild(createEle('button','Zerar','zerarBtn'))
+document.querySelector('.opcoestd').children[1].children[0].appendChild(createEle('button','NaN','confBtn'))
 
 //*************************** Stilizando ***************************//
 
-document.querySelector('.StatusLab').style.cssText = 'margin: 50px;' + 'font-weight: bold;'
+document.querySelector('.StatusLab').style.cssText = 'margin: 35%;' + 'font-weight: bold;'
 document.querySelector('.pausarBtn').style.cssText = 'margin: 2px;'
 document.querySelector('.iniciarBtn').style.cssText = 'margin: 2px;'
 document.querySelector('.zerarBtn').style.cssText = 'margin: 2px;'
+document.querySelector('.confBtn').style.cssText = 'margin: 2px;'
 document.querySelector('.opcoestd').children[0].children[0].colSpan = '2';
 document.querySelector('.opcoestd').children[0].children[0].style.cssText = 'padding-top: 18px;' + 'padding-bottom: 10px;'
-document.querySelector('.opcoestd').style.cssText = 'padding-bottom: 500px;'
+document.querySelector('.opcoestd').style.cssText = 'padding-top: 200px;'+'position: absolute;'
 document.querySelector('.opcoestd').children[0].style.cssText = 'background-color: white;'
 document.querySelector('.opcoestd').children[1].style.cssText = 'background-color: white;'
 
@@ -103,8 +106,50 @@ document.querySelector('.zerarBtn').addEventListener('click',function(){
     document.querySelector('.StatusLab').style.cssText += 'color: red;'
     resetar();
     format2 = (new Intl.NumberFormat('dec', { style: 'decimal' }).format(total));
-    $('#content_value').find('h2').html('[Auto Coleta] Criado por Foxkrak</br></br>Tempo:' + ' ' + timer1(ss) + '⁣⁣      ' + 'Total de Recurso Coletado: ' + format2 + '</br></br>' + 'Madeira: ' + format3 + '&nbsp;&nbsp;&nbsp;Argila: ' + format3+ '&nbsp;&nbsp;&nbsp;Ferro: ' + format3)
+    $('#content_value').find('h2').html('[Auto Coleta] Criado por Foxkrak</br></br>Tempo:' + ' ' + timer1(ss) + '⁣⁣      ' + 'Total de Recurso Coletado: ' + format2 + '</br></br>' + 'Madeira: ' + format3 + '&nbsp;&nbsp;&nbsp;Argila: ' + format3+ '&nbsp;&nbsp;&nbsp;Ferro: ' + format3)
 })
+document.querySelector('.confBtn').addEventListener('click',function(){
+    verificaconfbtn();
+})
+let status = JSON.parse(localStorage.getItem('Status'))
+if(status === 1){
+    document.querySelector('.confBtn').innerText = 'Ligado';
+    let stringJSON = JSON.stringify({"props":{"ASS":{"troopsAssigner":{"mode":"addict","allowedOptionIds":[1,2,3,4],"targetDurationSeconds":7200,"troops":{"spear":{"maySend":true,"reserved":0},"sword":{"maySend":true,"reserved":0},"axe":{"maySend":true,"reserved":0},"archer":{"maySend":true,"reserved":0},"light":{"maySend":true,"reserved":0},"marcher":{"maySend":true,"reserved":0},"heavy":{"maySend":true,"reserved":0},"knight":{"maySend":true,"reserved":0}},"troopOrder":[["axe","light","marcher"],["spear","sword","archer"],["heavy"],["knight"]]}}}});
+    localStorage.setItem('twcheese.userConfig', stringJSON)
+    document.querySelector('.confBtn').title = 'Mesmo Tempo de coleta em todos';
+    confbtn = true;
+}else if(status === 0){
+    document.querySelector('.confBtn').innerText = 'Desligado';
+    let stringJSON = JSON.stringify({"props":{"ASS":{"troopsAssigner":{"mode":"sane_person","allowedOptionIds":[1,2,3,4],"targetDurationSeconds":7200,"troops":{"spear":{"maySend":true,"reserved":0},"sword":{"maySend":true,"reserved":0},"axe":{"maySend":true,"reserved":0},"archer":{"maySend":true,"reserved":0},"light":{"maySend":true,"reserved":0},"marcher":{"maySend":true,"reserved":0},"heavy":{"maySend":true,"reserved":0},"knight":{"maySend":true,"reserved":0}},"troopOrder":[["axe","light","marcher"],["spear","sword","archer"],["heavy"],["knight"]]}}}});
+    localStorage.setItem('twcheese.userConfig', stringJSON)
+    document.querySelector('.confBtn').title = 'Coletar o Maximo possivel na melhor coleta';
+    confbtn = false;
+}
+
+function verificaconfbtn(){
+    let status = JSON.parse(localStorage.getItem('Status'))
+    if(status === null || status === undefined){
+        let stringJSON = JSON.stringify(1);
+        localStorage.setItem('Status', stringJSON)
+        confbtn = true;
+    }else if(status === 0){
+        document.querySelector('.confBtn').innerText = 'Ligado';
+        let stringJSON = JSON.stringify({"props":{"ASS":{"troopsAssigner":{"mode":"addict","allowedOptionIds":[1,2,3,4],"targetDurationSeconds":7200,"troops":{"spear":{"maySend":true,"reserved":0},"sword":{"maySend":true,"reserved":0},"axe":{"maySend":true,"reserved":0},"archer":{"maySend":true,"reserved":0},"light":{"maySend":true,"reserved":0},"marcher":{"maySend":true,"reserved":0},"heavy":{"maySend":true,"reserved":0},"knight":{"maySend":true,"reserved":0}},"troopOrder":[["axe","light","marcher"],["spear","sword","archer"],["heavy"],["knight"]]}}}});
+        localStorage.setItem('twcheese.userConfig', stringJSON)
+        let strJSON = JSON.stringify(1);
+        localStorage.setItem('Status', strJSON)
+        document.querySelector('.confBtn').title = 'Mesmo Tempo de coleta em todos';
+        confbtn = true;
+    }else if(status === 1){
+        document.querySelector('.confBtn').innerText = 'Desligado';
+        let stringJSON = JSON.stringify({"props":{"ASS":{"troopsAssigner":{"mode":"sane_person","allowedOptionIds":[1,2,3,4],"targetDurationSeconds":7200,"troops":{"spear":{"maySend":true,"reserved":0},"sword":{"maySend":true,"reserved":0},"axe":{"maySend":true,"reserved":0},"archer":{"maySend":true,"reserved":0},"light":{"maySend":true,"reserved":0},"marcher":{"maySend":true,"reserved":0},"heavy":{"maySend":true,"reserved":0},"knight":{"maySend":true,"reserved":0}},"troopOrder":[["axe","light","marcher"],["spear","sword","archer"],["heavy"],["knight"]]}}}});
+        localStorage.setItem('twcheese.userConfig', stringJSON)
+        let strJSON = JSON.stringify(0);
+        localStorage.setItem('Status', strJSON)
+        document.querySelector('.confBtn').title = 'Coletar o Maximo possivel na melhor coleta';
+        confbtn = false;
+    }
+}
 
 function verifica(){
     if(localStorage.getItem('AutoColeta') === '1'){
@@ -137,25 +182,40 @@ function StartS(){
         totalDiv3 = Math.round(total / 3);
         format3 = (new Intl.NumberFormat('dec', { style: 'decimal' }).format(totalDiv3));
         format2 = (new Intl.NumberFormat('dec', { style: 'decimal' }).format(total));
-        $('#content_value').find('h2').html('[Auto Coleta] Criado por Foxkrak</br></br>Tempo:' + ' ' + timer1(ss) + '⁣⁣      ' + 'Total de Recurso Coletado: ' + format2 + '</br></br>' + 'Madeira: ' + format3 + '&nbsp;&nbsp;&nbsp;Argila: ' + format3+ '&nbsp;&nbsp;&nbsp;Ferro: ' + format3)
+        $('#content_value').find('h2').html('[Auto Coleta] Criado por Foxkrak</br></br>Tempo:' + ' ' + timer1(ss) + '⁣⁣      ' + 'Total de Recurso Coletado: ' + format2 + '</br></br>' + 'Madeira: ' + format3 + '&nbsp;&nbsp;&nbsp;Argila: ' + format3+ '&nbsp;&nbsp;&nbsp;Ferro: ' + format3)
         var recaptcha = document.getElementsByClassName('recaptcha-checkbox-checkmark');
         if (recaptcha.length != 0){
             document.documentElement.getElementsByClassName('recaptcha-checkbox-checkmark')[0].click();
         }
         var listabtn = document.querySelectorAll('.free_send_button');
-        var relogio = document.getElementsByClassName('return-countdown');
         let i = document.querySelectorAll('.free_send_button').length;
         let l = i-1
-        if(document.querySelector('#loading_content').style.display === 'none' && document.querySelectorAll('.return-countdown').length <= 4){
-            for(let itens of document.querySelectorAll('.units-entry-all')){
-                if(parseInt(itens.innerText.replace(/[^0-9]/g,'')) >= 10){
-                    twcheese1();
+        if(confbtn){
+            if(document.querySelector('#loading_content').style.display === 'none' && document.querySelectorAll('.return-countdown').length === 0){
+                for(let itens of document.querySelectorAll('.units-entry-all')){
+                    if(parseInt(itens.innerText.replace(/[^0-9]/g,'')) >= 10){
+                        twcheese1();
+                    }
+                }
+                if(document.querySelectorAll('.free_send_button').length > 0 && document.querySelectorAll('.wood-value')[l].innerText !== '0'){
+                    for(let k = 1; k <= i; k++) {
+                        task(k, l);
+                        l--
+                    }
                 }
             }
-            if(document.querySelectorAll('.free_send_button').length > 0 && document.querySelectorAll('.wood-value')[l].innerText !== '0'){
-                for(let k = 1; k <= i; k++) {
-                    task(k, l);
-                    l--
+        }else{
+            if(document.querySelector('#loading_content').style.display === 'none' && document.querySelectorAll('.return-countdown').length !== 0 && document.querySelectorAll('.return-countdown').length <= 4){
+                for(let itens of document.querySelectorAll('.units-entry-all')){
+                    if(parseInt(itens.innerText.replace(/[^0-9]/g,'')) >= 10){
+                        twcheese1();
+                    }
+                }
+                if(document.querySelectorAll('.free_send_button').length > 0 && document.querySelectorAll('.wood-value')[l].innerText !== '0'){
+                    for(let k = 1; k <= i; k++) {
+                        task(k, l);
+                        l--
+                    }
                 }
             }
         }
